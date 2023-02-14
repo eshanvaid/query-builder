e58430f110d615c8b4f5b4190d59ab6df0c960aa
Eshan <eshanvaid01@gmail.com>
2023-02-14 22:59:58 +0530

    name fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import QueryBuilder from './components/Builder';
+import Builder from './components/Builder';
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-[#1D2025]">
         <Navbar />
-        <QueryBuilder />
+        <Builder />
       </div>
     </>
   );
